6cbce61b0408a5b34ea0dbe85066bc3198f32bf3
vandasel <kubavan02@gmail.com>
2024-09-03 21:05:19 +0100

    cleared main for future tasks

--- app/main.py
+++ app/main.py
@@ -1,22 +1,6 @@
 import chromadb
 from chromadb.utils import embedding_functions
-from chromadb.config import Settings
-
-chroma_client = chromadb.HttpClient(host='chroma_docker',port=8000)
-
-collection = chroma_client.create_collection(name="test_collection")
-collection.add(
-    documents=["doc1", "doc2", "doc3"],
-    embeddings=[[1.1, 2.3, 3.2], [4.5, 6.9, 4.4], [1.1, 2.3, 3.2]],
-    ids=['id1','id2','id3']
-)
-
-result = collection.query(
-    query_embeddings=[[1.1, 2.3, 3.2]],
-    n_results=3,
-)
-print(result.get('distances',[]))
-
+from chromadb.config import Setting
 
 
 print()
