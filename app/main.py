b7cd84d378e6104b9f7f27389fd290a9c243830e
vandasel <kubavan02@gmail.com>
2024-09-03 16:26:29 +0100

    complete configuration with docker network + basic showcase of distances

---
+++ app/main.py
@@ -0,0 +1,19 @@
+import chromadb
+from chromadb.utils import embedding_functions
+from chromadb.config import Settings
+
+chroma_client = chromadb.Client()
+
+collection = chroma_client.create_collection(name="my_collection")
+collection.add(
+    documents=["doc1", "doc2", "doc3"],
+    embeddings=[[1.1, 2.3, 3.2], [4.5, 6.9, 4.4], [1.1, 2.3, 3.2]],
+    ids=['id1','id2','id3']
+)
+
+result = collection.query(
+    query_embeddings=[[1.1, 2.3, 3.2]],
+    n_results=3,
+)
+print(result.get('distances',[]))
+print()
