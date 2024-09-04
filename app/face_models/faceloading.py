31f6bf744907f266459e8b7773f6b2069be72c7e
vandasel <kubavan02@gmail.com>
2024-09-04 15:54:48 +0100

    added FaceLoader

--- app/face_models/faceloading.py
+++ app/face_models/faceloading.py
@@ -1,16 +1,45 @@
-from mtcnn import MTCNN
-import os
 import cv2 as cv
-
+from mtcnn import MTCNN
+import matplotlib.pyplot as plt # for checking results
+import os
+import re
 
 class FaceLoader():
     def __init__(self, path : str):
         self.path = path
         self.detector = MTCNN()
+        self.images = []
+        
+    def get_paths(self):
+        paths = []
+        for folder in os.listdir(self.path):
+            folders = os.listdir(self.path +"/" + folder)
+            for image in folders:
+                path = self.path + "/" + folder+"/"+image
+                paths.append(path)
+        return paths
+                    
+    def get_faces(self):
+        out = {}
+        for path in self.get_paths():
+            image = cv.imread(path)
+            img = cv.cvtColor(image,cv.COLOR_BGR2RGB)
+            x,y,w,h = self.detector.detect_faces(img)[0].get('box',[])
+            x,y = abs(x),abs(y)
+            face = img[y:y+h,x:x+w]
+            face_w = cv.resize(face,(160,160))
+            group = re.search(r'\/pins_(.*)\/',path)
+            if group:
+                key = group.group(1)
+                if key in out:
+                    out[key].append(face_w)
+                else:
+                    out[key] = [face_w]
+        return out
 
-    def load_images(self):
-        ...
 
     def run(self):
-        ...    
+        return self.get_faces()
 
+
+print()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import chromadb
 from chromadb.utils import embedding_functions
 from chromadb.config import Setting
+from face_models.faceloading import FaceLoader
+
 
 
 print()
