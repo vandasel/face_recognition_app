75998e3b0ab49564c4dc407ed969f2da3e6ee0d6
vandasel <kubavan02@gmail.com>
2024-09-21 22:17:24 +0100

    changes to model, different approach

--- app/face_models/model_mtcnn.py
+++ app/face_models/model_mtcnn.py
@@ -1,42 +1,42 @@
-import cv2 as cv
-from facenet_pytorch import MTCNN
-import matplotlib.pyplot as plt # for checking results
-import re
 import torch
+from PIL import Image
+import re
 import logging
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
 
 class FaceLoader():
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     def __init__(self, paths:list):
         self.paths = paths
-        self.detector = MTCNN(keep_all=True, device=self.device, post_process=False)
+        self.detector = MTCNN(image_size=160, device=self.device)  
+        self.embedder = InceptionResnetV1(pretrained='casia-webface').eval()
         self.images = []
-
-                    
     def get_faces(self) -> dict:
-        count=0
+        count = 0
         out = {}
         for path in self.paths:
-            image = cv.imread(path)
-            img = cv.cvtColor(image,cv.COLOR_BGR2RGB)
+            img = Image.open(path)
             face = self.detector.detect(img)
-            if face[0] is not None and face[0][0].tolist():
-                count+=1
-                x,y,w,h = face[0][0]
-                x,y = abs(x),abs(y)
-                x,y,w,h = int(x),int(y),int(w),int(h)
-                face = img[x:x+w,y:y+h]
-                face_w = cv.resize(face,(160,160))
-                group = re.search(r'\/pins_(.*)\/',path)
+            if face is not None:
+                aligned = self.detector(img)
+                img.close()
+                if aligned is not None:
+                    aligned = aligned.unsqueeze(0)
+                    with torch.no_grad():
+                        embedding = self.embedder(aligned) 
+                    count += 1
+                    group = re.search(r'\/pins_(.*)\/', path)
+                    if group:
+                        key = group.group(1)
+                        if key in out:
+                            out[key].append(embedding)
+                        else:
+                            out[key] = [embedding]
+            if count % 10 == 0:
+                torch.cuda.empty_cache()
                 logging.info(f"image {count}")
-                if group:
-                    key = group.group(1)
-                    if key in out:
-                        out[key].append(face_w)
-                    else:
-                        out[key] = [face_w]
         return out
-
 
     def run(self):
         return self.get_faces()
--- web_app/face_models/model_mtcnn.py
+++ web_app/face_models/model_mtcnn.py
@@ -1,28 +1,26 @@
 import cv2 as cv
-from facenet_pytorch import MTCNN
+from facenet_pytorch import MTCNN, InceptionResnetV1
 import numpy as np
-from keras_facenet import FaceNet
 import re
 import torch
 
 class FaceLoader():
-    embedder = FaceNet()
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     def __init__(self, image):
         self.image = image
-        self.detector = MTCNN(keep_all=True, device=self.device, post_process=False)      
+        self.detector = MTCNN(keep_all=True, device=self.device, post_process=False)  
+        self.embedder = InceptionResnetV1(pretrained='vggface2').eval().to(self.device)    
     def face_embbed(self):
         face = self.detector.detect(self.image)
         if face[0] is not None and face[0][0].tolist():
-            x,y,w,h = face[0][0]
-            x,y = abs(x),abs(y)
-            x,y,w,h = int(x),int(y),int(w),int(h)
-            face = self.image[x:x+w,y:y+h]
-            face_w = cv.resize(face,(160,160))
-            face_image = face_w.astype('float32')
-            face_image = np.expand_dims(face_image,axis=0)
-            out_image = self.embedder.embeddings(face_image)
-        return out_image
+            face = self.detector.detect(self.image)
+            if face[0] is not None and face[0][0].tolist():
+                aligned = self.detector(self.image)
+                with torch.no_grad():  
+                    aligned = aligned.to(self.device)  
+                    face_e = self.embedder(aligned).detach().cpu()
+
+        return face_e.tolist()[0]
 
     def run(self):
         return self.face_embbed()
