b7d5b909ee7aa9093f5696a787fb01d8addfff96
vandasel <kubavan02@gmail.com>
2024-10-08 15:44:22 +0100

    +

--- app/calculator.py
+++ app/calculator.py
@@ -3,17 +3,16 @@ def calculate_dict(TP,TN,FP,FN):
             "Accuracy" : (TP+TN)/(TP+TN+FP+FN),
             "Precision" : TP/(TP+FP) if TP+FP!=0 else 0,
             "Recall" : TP/(TP+FN) if TP+FN!=0 else 0,
-            "Specifity" : TN/(TN+FP) if TN+FP!=0 else 0
+            "Specifity" : TN/(TN+FP) if TN+FP!=0 else 0,
+            
         }
 
 def get_best(dictt:dict):
     maks = 0
     best = ""
     for threshold,metrics in dictt.items():
-        result = 0
-        for metric in metrics.values():
-            result+=metric
-        if result>maks:
-            maks = result
+        if metrics.get("Accuracy")>maks:
+            maks = metrics.get("Accuracy")
             best = threshold
-    return best
+    return [float(best)]
+
